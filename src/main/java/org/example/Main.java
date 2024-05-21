219c88ed3f102e8aedbce044e636177e5b4c9d95
aymane <houri.aymane03@gmail.com>
2024-05-22 00:10:28 +0100

    nouveau code de test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,34 +3,9 @@ package org.example;
 import java.util.Scanner;
 
 public class Main {
-
-    public static void main(String[] args) {
-        // Créer un scanner pour la saisie utilisateur
-        Scanner scanner = new Scanner(System.in);
-
-        // Demander à l'utilisateur de saisir deux nombres
-        System.out.print("Entrez le premier nb : ");
-        double premierNombre = scanner.nextDouble();
-
-        System.out.print("Entrez le deuxième nombre : ");
-        double deuxiemeNombre = scanner.nextDouble();
-
-        // Fermer le scanner après avoir obtenu les entrées de l'utilisateur
-        scanner.close();
-
-        // Calculer la somme des deux nombres
-        double somme = premierNombre + deuxiemeNombre;
-
-        // Tester si la somme est un nombre entier
-        if (estNombreEntier(somme)) {
-            System.out.println("La somme est un nombre entier : " + (int) somme);
-        } else {
-            System.out.println("La somme n'est pas un nombre entier : " + somme);
-        }
+    public int add(int a, int b) {
+        return a + b;
     }
-
-    // Fonction pour tester si un nombre est entier
-    private static boolean estNombreEntier(double nombre) {
-        return nombre % 1 == 0;
+    public static void main(String[] args) {
     }
 }
---
+++ src/test/java/test.java
@@ -0,0 +1,16 @@
+import org.example.Main;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class test {
+
+    @Test
+    public void testAdd() {
+        Main main = new Main();
+        assertEquals(5, main.add(2, 3), "2 + 3 devrait être égal à 5");
+    }
+
+    }
+
+
+
