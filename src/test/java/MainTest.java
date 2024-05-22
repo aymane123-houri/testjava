e70fc6db1818527b401db252a6f028ed2f3b36ee
aymane <houri.aymane03@gmail.com>
2024-05-22 01:09:40 +0100

    nouveau code de test7

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -2,12 +2,12 @@ import org.example.Main;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
-public class test {
+public class MainTest {
 
     @Test
     public void testAdd() {
         Main main = new Main();
-        assertEquals(5, main.add(3, 3), "2 + 3 devrait être égal à 5");
+        assertEquals(5, main.add(2, 3), "2 + 3 devrait être égal à 5");
     }
 
     }
